6cb3300dda1dc48aa5d33296766027c0dc86c028
NO-243PCBER03I9\ENP <ximik-xim@mail.ru>
2025-06-15 15:31:10 +0300

    - Добавление поддержки TList(0.6.0) - Добавление бэкапов

--- Characteristic And Bust/Custom Bust Logic Lite/Bust Data/Float/Key Bust Data/TList/IdentifierAndData_KeyBustData.cs
+++ Characteristic And Bust/Custom Bust Logic Lite/Bust Data/Float/Key Bust Data/TList/IdentifierAndData_KeyBustData.cs
@@ -12,4 +12,16 @@ public class IdentifierAndData_KeyBustData : AbsIdentifierAndData<IndifNameSO_Ke
  {
   return _dataKey;
  }
+ 
+#if UNITY_EDITOR
+ public override string GetJsonSaveData()
+ {
+  return JsonUtility.ToJson(_dataKey);
+ }
+
+ public override void SetJsonData(string json)
+ {
+  _dataKey = JsonUtility.FromJson<KeyBustData>(json);
+ }
+#endif
 }
--- Characteristic And Bust/Custom Bust Logic Lite/Filter/Key Filter Bust/TList/IdentifierAndData_KeyFilterBust.cs
+++ Characteristic And Bust/Custom Bust Logic Lite/Filter/Key Filter Bust/TList/IdentifierAndData_KeyFilterBust.cs
@@ -12,4 +12,16 @@ public class IdentifierAndData_KeyFilterBust : AbsIdentifierAndData<IndifNameSO_
  {
   return _dataKey;
  }
+ 
+#if UNITY_EDITOR
+ public override string GetJsonSaveData()
+ {
+  return JsonUtility.ToJson(_dataKey);
+ }
+
+ public override void SetJsonData(string json)
+ {
+  _dataKey = JsonUtility.FromJson<KeyFilterBust>(json);
+ }
+#endif
 }
